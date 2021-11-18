9fcdb26a95797ba93cad4c15e443273f06757e28
JOANNE CHEUNG (PDM-CS/HKG) <CHEUNJO2@OOCL.COM>
2021-11-18 13:29:05 +0800

    complete tasks in index.js

--- index.js
+++ index.js
@@ -2,25 +2,58 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+var ans = [];
+a.forEach(ele => ans.push(ele*2));
+console.log(ans);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+console.log(colors.join(" "));  //case 1
+console.log(colors.join("+"));  //case 2
+console.log(colors.join(","));  //case 3
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+function sorting(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let flag = arr[0];
+    const smaller = [];
+    const bigger = [];
+    arr.forEach(ele => {
+        if (ele < flag) {
+            smaller.push(ele);
+        } else if (ele > flag) {
+            bigger.push(ele);
+        }
+    });
 
+    return [...sorting(bigger), flag, ...sorting(smaller)];
+}
+console.log(sorting(a));
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var ans = [];
+var maxEle = a[0];
+for(let j = 0; j < a.length; j++) {
+    if (ans[a[j]]) {
+        ans[a[j]]++;
+    } else ans[a[j]] = 1;
+    if (ans[a[j]] > ans[maxEle]) {
+        maxEle = a[j];
+    }
+}
+console.log(maxEle);
